a9d1ff2c81092acccf1cce739d59ea84f3099b37
Anthony89 <anthony.kim.89@hotmail.com>
2016-10-04 02:33:13 -0700

    no need to decorate the routing in auth.html

--- src/app/components/auth/router.decorator.js
+++ src/app/components/auth/router.decorator.js
@@ -3,7 +3,9 @@
 (function() {
 
   angular.module('GasNinjasAdmin.components.auth')
-    .run(function($rootScope, $state, Auth) {
+    .run(function($rootScope, $state, $location, Auth) {
+      if ($location.absUrl().indexOf('auth.html') != -1 || $location.absUrl().indexOf('reg.html') != -1)
+        return;
       // Redirect to login if route requires auth and the user is not logged in, or doesn't have required role
       $rootScope.$on('$stateChangeStart', function(event, next) {
         if (!next.authenticate) {
