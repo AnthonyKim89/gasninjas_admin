21877dd8fbcc8663ce8bad6909430315da1ae2dc
anthony89 <anthony.kim.89@hotmail.com>
2017-10-12 10:28:02 -0700

    refactor(Routing): do not check the authentication now that we have dynamic menu rendering.

--- src/app/components/auth/router.decorator.js
+++ src/app/components/auth/router.decorator.js
@@ -6,8 +6,12 @@
     .run(function($rootScope, $state, $location, Auth) {
       if ($location.absUrl().indexOf('auth.html') != -1 || $location.absUrl().indexOf('reg.html') != -1)
         return;
+      
+      // Commented out by Anthony on the Oct 12, 2017
+      // This is no longer required because we generate state dynamically based on the user permission
+      
       // Redirect to login if route requires auth and the user is not logged in, or doesn't have required role
-      $rootScope.$on('$stateChangeStart', function(event, next) {
+      /*$rootScope.$on('$stateChangeStart', function(event, next) {
         if (!next.authenticate) {
           return;
         }
@@ -45,6 +49,6 @@
       function fnForceLogin(event) {
         event.preventDefault();
         location.href = 'auth.html';
-      }
+      }*/
     });
 })();
