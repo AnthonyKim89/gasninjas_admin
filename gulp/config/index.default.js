15c7f01872e03069b6dde0baee479a06a95e7a4d
anthony89 <anthony.kim.89@hotmail.com>
2017-10-19 23:05:07 -0700

    fix(Gulp): list an example config in the index.default.js

--- gulp/config/index.default.js
+++ gulp/config/index.default.js
@@ -3,6 +3,7 @@
  */
 module.exports = {
   dist: 'dist',
+  // dist: '/var/www/html/admin',
   apiServer: 'http://localhost/gn_api',
   // apiServer: 'https://devdb.gasninjas.com/gn_api',
   // apiServer: 'https://api-miami.gasninjas.com/gn_api',
